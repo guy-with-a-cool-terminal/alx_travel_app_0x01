9a156ef2e5236f02ee49b4a97665db4a79cbfa43
guy-with-a-cool-terminal <njugunabriian@gmail.com>
2025-07-17 20:30:22 +0300

    yikes

--- alx_travel_app/listings/serializers.py
+++ alx_travel_app/listings/serializers.py
@@ -1,6 +1,5 @@
-from rest_framework import serializers
 from .models import Listing, Booking
-
+from rest_framework import serializers
 
 class BookingSerializer(serializers.ModelSerializer):
     class Meta:
